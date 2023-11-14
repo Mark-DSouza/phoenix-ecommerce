f53957a00e6a30358bbc71d9880aec5bd6e4e2b3
Mark-DSouza <markdsouza434@gmail.com>
2023-11-14 21:26:53 +0530

    Incorporate the category resource into products

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+for category <- ["Home Improvement", "Power Tools", "Gardening", "Books", "Education"] do
+  {:ok, _} = Hello.Catalog.create_category(%{title: category})
+end
